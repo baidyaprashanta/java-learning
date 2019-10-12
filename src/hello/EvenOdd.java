54ea6cdc87787a6fdc466a0d4fb38450da4ca942
baidyaprashanta <baidyaprashanta@gmail.com>
2019-10-11 23:25:40 -0400

    this is my first commit

---
+++ src/hello/EvenOdd.java
@@ -0,0 +1,15 @@
+package hello;
+
+public class EvenOdd {
+
+	public static void main(String[] args) {
+		int a=5;
+		if (a%2==0) {
+			System.out.println("it is an even number");
+		}else {
+			System.out.println("it is an odd number");
+		}
+
+	}
+
+}
---
+++ src/hello/Intro.java
@@ -0,0 +1,11 @@
+package hello;
+
+public class Intro {
+
+	public static void main(String[] args) {
+		System.out.println("Hi! This is Prashanta");
+		System.out.println("I am fron nepal");
+
+	}
+
+}
